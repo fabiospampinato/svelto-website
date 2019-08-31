e9696972f432acbafd0479e571dbd8d0d3c97875
Fabio Spampinato <spampinabio@gmail.com>
2019-08-31 16:38:33 +0200

    Improved homepage title

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Layout from '@layouts/layout';
 /* INDEX */
 
 const Index = () => (
-  <Layout className="home" seo={{ title: 'Home' }}>
+  <Layout className="home" seo={{ title: 'Svelto', titleTemplate: '%s' }}>
     <img className="logo" src="/static/images/logo.png" title="Logo" />
     <Button className="circular demo" href="//demo.getsvelto.com">DEMO</Button>
     <Button className="circular docs" href="/docs">DOCS</Button>
